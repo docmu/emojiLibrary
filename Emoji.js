ff03f8c4b018ea3b4f448d21d4e39d419d0a0d07
docmu <docm@mymail.vcu.edu>
2020-05-29 13:06:09 -0400

    delete commented out code

--- Emoji.js
+++ Emoji.js
@@ -6,31 +6,6 @@
 
   // hidden within the scope of the IIFE and never directly accessible
   //available emojis
-  // var supportedEmojis = [
-  //   "shrug",
-  //   "acid",
-  //   "kiss",
-  //   "danceParty",
-  //   "flowerGirl",
-  //   "Lenny",
-  //   "tableFlip",
-  //   "sad",
-  //   "happy",
-  //   "zombie",
-  //   "chineseMafia",
-  //   "confused",
-  //   "embarrassed",
-  //   "angry",
-  //   "magic",
-  //   "booty",
-  //   "takeMyMoney",
-  //   "crazy",
-  //   "disappointment",
-  //   "cool",
-  //   "flex",
-  //   "middleFinger",
-  //   "",
-  // ];
   var emojis = {
     shrug: "¯\\_(ツ)_/¯",
     acid: "⊂(◉‿◉)つ",
@@ -55,31 +30,6 @@
     flex: "ᕦ(ò_óˇ)ᕤ",
     middleFinger: "┌П┐(ಠ_ಠ)",
   };
-  // //the emojis in ascii
-  // var asciiEmojis = [
-  //   "¯\\_(ツ)_/¯",
-  //   "⊂(◉‿◉)つ",
-  //   "(づ￣ ³￣)づ",
-  //   "♪┏(・o･)┛♪┗ ( ･o･) ┓♪",
-  //   "(◕‿◕✿)",
-  //   "( ͡° ͜ʖ ͡°)",
-  //   "(╯°□°)╯︵ ┻━┻",
-  //   "(╥﹏╥)",
-  //   "(＾∇＾)",
-  //   "[¬º-°]¬",
-  //   "(-(-.(-.(-.-).-).-)-)",
-  //   "(⊙.☉)7",
-  //   "(⁄⁄•⁄ω⁄•⁄⁄)",
-  //   "(╬ ಠ益ಠ)",
-  //   "(∩｀-´)⊃━☆ﾟ.*･｡ﾟ",
-  //   "(‿|‿)",
-  //   "(╯°□°）╯$ $ $",
-  //   "(⊙_◎)",
-  //   "ಠ_ಠ",
-  //   "(•_•) ( •_•)>⌐■-■ (⌐■_■)",
-  //   "ᕦ(ò_óˇ)ᕤ",
-  //   "┌П┐(ಠ_ಠ)",
-  // ];
 
   //prototype holds methods callable by Emoji objects (to save memory space)
   Emoji.prototype = {
@@ -87,29 +37,25 @@
       //check if argument passed is a valid emoji
       //valid arguments can either be a key within the emojis object or an empty string
       //references the externally inaccessible 'emojis' object within the closure
-      //if (supportedEmojis.indexOf(this.name) === -1) {
       if (!emojis.hasOwnProperty(this.name) && this.name !== "") {
         throw "invalid emoji";
       }
     },
 
-    //returns the the ascii Emoji corresponding to the name passed as an argument
+    //returns the the value corresponding to the key passed as an argument
+    //else, empty string return a random emoji
     translate: function () {
       this.validate();
 
       if (this.name !== "") {
-        //return ascii translation of argument passed
-        //return asciiEmojis[supportedEmojis.indexOf(this.name)];
         return emojis[this.name];
       } else {
-        //else, empty string return a random emoji
         return this.random();
       }
     },
 
     //return a random Emoji
     random: function () {
-      // return asciiEmojis[Math.floor(Math.random() * asciiEmojis.length)];
       var keys = Object.keys(emojis);
       return emojis[keys[(keys.length * Math.random()) << 0]];
     },
